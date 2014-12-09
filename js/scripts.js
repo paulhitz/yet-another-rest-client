8940aa86153e8364b56764f5f5b435b50a9924c9
Paul Hitz <hitzp@dnb.com>
2014-12-09 19:58:43 +0000

    Test Authentication Service
    Add some caching

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,82 @@
 
-
 var clientApp = angular.module('clientApp', []);
 
-clientApp.factory('Data', function () {
-  return { message: "test" };
+
+
+clientApp.controller('ClientController', function($scope, AuthService) {
+
+
+
+
+  $scope.authorise = function() {
+	  AuthService.getAuthCookie().then(
+		  function(payload) {
+			console.log("headers = " + payload.authorization);
+			$scope.authenticationCookie = payload.authorization;
+		  },
+		  function(error) {
+			console.log("error = " + error);
+		  }
+	  );
+  }
+
 });
 
 
 
 
-function ClientController($scope, Data) {
+clientApp.factory('AuthService', function($http, $q) {
+  var cachedAuthCookie = "";
 
-  //$scope.authenticationCookie = "safdsfs";
-  //$scope.requestUrl = "dsfdsf";
+  return {
+    getAuthCookie: function() {
+      var deferred = $q.defer();
 
-  //$scope.data = Data;
-  
-  $scope.test = function() {
-    //alert("testing");
-	$scope.data = Data;
+	  if (cachedAuthCookie !== "") {
+	    deferred.resolve({authorization: cachedAuthCookie});
+	  } else {
+		var AUTHENTICATION_REQUEST_CONFIG = { headers: {
+			'ApplicationId': '36',
+			'x-dnb-user': 'teamjoly@dnb.com',
+			'x-dnb-pwd': 'password'
+		}};
+		var STG_AUTHENTICATION_URL = 'http://services-ext-stg.dnb.com/rest/Authentication';
+
+		$http.get(STG_AUTHENTICATION_URL, AUTHENTICATION_REQUEST_CONFIG).
+		success(function(data, status, headers, config) {
+			cachedAuthCookie = headers('authorization');
+			deferred.resolve({authorization: headers('authorization')});
+		}).
+		error(function(msg, code) {
+			deferred.reject("Error Code: " + code);
+			//$log.error(msg, code);
+		});		
+	  }	 
+	 
+     return deferred.promise;
+   }
   }
-}
+ });
+
+
+
+
+
+
+
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
 
 
 function CallService($scope, $http) {
@@ -33,7 +89,7 @@ function CallService($scope, $http) {
 	};
 
 	var serviceUrl = 'http://services-ext-stg.dnb.com/rest/LinkageService/V2/OrderProduct?DUNSNumber=211528187&CountryISOAlpha2Code=GB&DNBProductID=LNK_FF_MNRT';
-	
+
 	//TODO consider using a $promise
     $http.get(serviceUrl, requestConfig).
 		success(function(data, status, headers, config) {
@@ -49,6 +105,78 @@ function CallService($scope, $http) {
             console.log(JSON.stringify(config));
 		});
 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function Authenticate3($http) {
+alert("4");
+	var AUTHENTICATION_REQUEST_CONFIG = { headers: {
+			'ApplicationId': '36',
+			'x-dnb-user': 'teamjoly@dnb.com',
+			'x-dnb-pwd': 'password'
+		}
+	};
+	var STG_AUTHENTICATION_URL = 'http://services-ext-stg.dnb.com/rest/Authentication';
+
+    var promise = $http.get(STG_AUTHENTICATION_URL, AUTHENTICATION_REQUEST_CONFIG);
+	
+	promise.then(
+	  function(payload) {
+	    alert("5");
+		console.log("headers = " + payload.headers('authorization'));
+		return payload.headers('authorization');
+	  },
+	  function(errorPayload) {
+		//TODO handle this
+	  });
+  
+alert("6");
+}
+
+
+function Authenticate2($http) {
+alert("1");
+	var AUTHENTICATION_REQUEST_CONFIG = { headers: {
+			'ApplicationId': '36',
+			'x-dnb-user': 'teamjoly@dnb.com',
+			'x-dnb-pwd': 'password'
+		}
+	};
+	var STG_AUTHENTICATION_URL = 'http://services-ext-stg.dnb.com/rest/Authentication';
+
+    $http.get(STG_AUTHENTICATION_URL, AUTHENTICATION_REQUEST_CONFIG).
+		success(function(data, status, headers, config) {
+		    //TODO need to use a promise here. are they angular's version of callbacks?
+			console.log("headers = " + headers('authorization'));
+			return headers('authorization');
+        }).
+		error(function(data, status, headers, config) {
+		    //TODO handle this
+		});
+//alert("2");
 }
 
 function Authenticate($scope, $http) {
@@ -70,8 +198,6 @@ function Authenticate($scope, $http) {
 		
 		});
 }
-
-
 
 
 
