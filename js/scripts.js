bc79e168bcf6c9627cdf81411dc8f669e39bc0a2
Paul Hitz <hitzp@dnb.com>
2014-12-09 17:14:43 +0000

    Test Authentication Service

--- js/scripts.js
+++ js/scripts.js
@@ -25,41 +25,50 @@ function ClientController($scope, Data) {
 
 function CallService($scope, $http) {
 
-//see cookiemanager.java
-/*
-        headers.set("x-dnb-user", username);
-        headers.set("x-dnb-pwd", password);
-        headers.set("ApplicationId", "36");
+	//TODO consider using config() to setup the $httpProvider and include headers there.
+	var requestConfig = { headers: {
+			'Authorization': '5Ccr7Qs6ZkEHbAgLMlDeg0ykUF%2F1Cb%2FxIcULRaUhRnaKR%2BvInWkbuNH6kv8MOkxYMc1qdWKm1apyxiNQe7cLXuogOCM4XQTHsa7u8BugcJaW53jklD70pSdZrKfYI24%2FwlQS6nxmYVvBM%2BZPZUY2DTbV5qZuvqd3GtUsy47PZahGyIJ6JxUW39hqw5sqHvSIqxsdDNPgUzG7UWvze2uwZh%2Bn2y5YjaBNMFHt3QoPoL5At88Re3P0tNCoMZ92bUAqdkLQnG2YnnTnp6wq%2FufVF6%2BKB82Cd1mwHcb%2BFFGicxyertcPOms5DM4OI7Mz%2F%2B%2B%2F7HkbLgAyH5j%2FJLj8Ig3tbg%3D%3D',
+			'ApplicationId': '36'
+		}
+	};
 
-*/
+	var serviceUrl = 'http://services-ext-stg.dnb.com/rest/LinkageService/V2/OrderProduct?DUNSNumber=211528187&CountryISOAlpha2Code=GB&DNBProductID=LNK_FF_MNRT';
+	
+	//TODO consider using a $promise
+    $http.get(serviceUrl, requestConfig).
+		success(function(data, status, headers, config) {
+            $scope.responseBody = data;
+			$scope.responseHeaders = headers();
+            console.log(status);
+            console.log(JSON.stringify(config));
+        }).
+		error(function(data, status, headers, config) {
+			$scope.responseBody = data;
+			$scope.responseHeaders = headers();
+            console.log(status);
+            console.log(JSON.stringify(config));
+		});
 
+}
 
+function Authenticate($scope, $http) {
 
+	var AUTHENTICATION_REQUEST_CONFIG = { headers: {
+			'ApplicationId': '36',
+			'x-dnb-user': 'teamjoly@dnb.com',
+			'x-dnb-pwd': 'password'
+		}
+	};
+	var STG_AUTHENTICATION_URL = 'http://services-ext-stg.dnb.com/rest/Authentication';
+	//var QA_AUTHENTICATION_URL = 'http://dbqlsegwb01.us.dnb.com:8080/rest/Authentication';
 
-	var config = {headers: {
-				'Authorization': 'M3b6kAJNEJWf3%2Frb4O8nie%2BW9s6uWoxvrwL3bY95BZloEykgWLzW9ZtPnwR%2BQNccsOlDBWx2UIrndD8NKmyaFqbntsazFDXm5MWNDmtZUE%2BDu9H45KVIHnKhbEPfxjH0b34fvf7MwVLjMuVdq0c5gphFC5v5SPNgfbDwxvAUkUWIdLBRHzmFQN9KM2hvWXethIfTeIIDZynl9YhDt3SrSHFvpr34i96wAWga3Imct23JU7tOQZ2vdJziL5lsfhH9lQJfyLrXlZ7pb7z493PE3BErQnzPgSzbmVUmtSKOgkm8AFweKDTX2HVv1SeYm6hUBVipMYgHZYZOn0oBd070AA%3D%3D'
-				
-			}
-		};
+    $http.get(STG_AUTHENTICATION_URL, AUTHENTICATION_REQUEST_CONFIG).
+		success(function(data, status, headers, config) {
+			$scope.authenticationCookie = headers('authorization');
+        }).
+		error(function(data, status, headers, config) {
 		
-		//'ApplicationId': '36'
-		//'Access-Control-Expose-Headers': 'X-Foo'
-		//'Accept': 'application/json;odata=verbose'
-		//Request header field X-Foo is not allowed by Access-Control-Allow-Headers.
-
-    $http.get('http://rest-service.guides.spring.io/greeting', config).
-        success(function(data, status, headers, config) {
-            $scope.response = data;
-			//alert("headers = " + headers);
-			//alert("config = " + config);
-			//alert("status = " + status);
-			$scope.headers = headers();
-			//$scope.headers = headers('Content-Type');
-			console.log(headers());
-			console.log(JSON.stringify(data));
-			console.log(data);
-            console.log(JSON.stringify(status));
-        });
+		});
 }
 
 
