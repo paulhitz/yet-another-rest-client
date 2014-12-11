b5f0f78dbed9c239a0067cc49f0eee539a064fa3
Paul Hitz <hitzp@dnb.com>
2014-12-11 19:41:04 +0000

    Call the configured and authorised endpoint

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ var clientApp = angular.module('clientApp', []);
 
 
 
-clientApp.controller('ClientController', function($scope, AuthService) {
+clientApp.controller('ClientController', function($scope, $http, AuthService) {
 
   //Populate the form.
   $scope.environments = servicesConfig.environments;
@@ -23,21 +23,25 @@ clientApp.controller('ClientController', function($scope, AuthService) {
     $scope.authorise();
 
 	//Determine the configured endpoint.
-    var endpoint = configureServiceUrl($scope);
+    var endpoint = configureServiceUrl($scope.environmentSelected, $scope.serviceSelected, $scope.duns);
+	$scope.requestUrl = endpoint;
 	console.log("configured endpoint to call = " + endpoint);
 
 	//Call the endpoint.
-	//TODO
+	//TODO this is being called before the auth promise is executed. move this below.
+	callService($scope, $http, endpoint);
   }
   
   $scope.authorise = function() {
-	  AuthService.getAuthCookie($scope.environmentSelected).then(
+      var authEndpoint = configureServiceUrl($scope.environmentSelected, "auth");
+  
+	  AuthService.getAuthCookie(authEndpoint).then(
 		  function(payload) {
-			//console.log("headers = " + payload.authorization);
+			console.log("headers = " + payload.authorization);
 			$scope.authenticationCookie = payload.authorization;
 		  },
 		  function(error) {
-			console.log("error = " + error);
+			console.log("error = " + error); //TODO handle this with error in the UI also.
 		  }
 	  );
   }
@@ -48,22 +52,22 @@ clientApp.controller('ClientController', function($scope, AuthService) {
 /**
  * Based on the selected environment and service, determine the correct URL to use.
  */
-function configureServiceUrl($scope) {
+function configureServiceUrl(environmentSelected, serviceSelected, dunsSelected) {
 	var url = "";
 	var duns = servicesConfig.placeholderDuns;
 
 	//Determine the endpoint based on selected Environment and Service.
 	for (var i in servicesConfig.endpoints) {
 		var endpoint = servicesConfig.endpoints[i];
-		if (endpoint.env === $scope.environmentSelected && endpoint.service === $scope.serviceSelected) {
+		if (endpoint.env === environmentSelected && endpoint.service === serviceSelected) {
 			url = endpoint.url;
 			break;
 		}
 	}
 
 	//Replace the DUNS placeholder.
-	if ($scope.duns) {
-		duns = replaceAll($scope.duns, "-", "");
+	if (dunsSelected) {
+		duns = replaceAll(dunsSelected, "-", "");
 	}
 	if (url) {
 		url = url.replace("{duns}", duns);
@@ -79,28 +83,28 @@ clientApp.factory('AuthService', function($http, $q) {
   var cachedAuthCookies = [];
 
   return {
-    getAuthCookie: function(environment) {
+    getAuthCookie: function(authEndpoint) {
 	  var deferred = $q.defer();
 
-	  if (typeof cachedAuthCookies[environment] !== 'undefined') {
-	    deferred.resolve({authorization: cachedAuthCookies[environment]});
+	  if (typeof cachedAuthCookies[authEndpoint] !== 'undefined') {
+	    deferred.resolve({authorization: cachedAuthCookies[authEndpoint]});
 	  } else {
 		var AUTHENTICATION_REQUEST_CONFIG = { headers: {
 			'ApplicationId': '36',
 			'x-dnb-user': 'teamjoly@dnb.com',
 			'x-dnb-pwd': 'password'
 		}};
-		var STG_AUTHENTICATION_URL = 'http://services-ext-stg.dnb.com/rest/Authentication'; //TODO determine endpoint based on env
 
-		$http.get(STG_AUTHENTICATION_URL, AUTHENTICATION_REQUEST_CONFIG).
+		console.log("authEndpoint = " + authEndpoint);
+		$http.get(authEndpoint, AUTHENTICATION_REQUEST_CONFIG).
 		success(function(data, status, headers, config) {
-			cachedAuthCookies[environment] = headers('authorization');
+			cachedAuthCookies[authEndpoint] = headers('authorization');
 			deferred.resolve({authorization: headers('authorization')});
 		}).
 		error(function(msg, code) {
 			deferred.reject("Error Code: " + code);
 			//$log.error(msg, code);
-		});		
+		});
 	  }
 
      return deferred.promise;
@@ -109,43 +113,25 @@ clientApp.factory('AuthService', function($http, $q) {
  });
 
 
- /**
-  * The name says it all.
-  */
-function replaceAll(input, target, replacement) {
-	return input.split(target).join(replacement);
-}
+/**
+ * Call the specified endpoint and update the UI.
+ *
+ * TODO:
+ * -the auth service requires different params
+ * -consider using config() to setup the $httpProvider and include headers there.
+ * -remove magic number. e.g. the app id.
+ * -can we reuse the auth service method?
+ */
+function callService($scope, $http, endpoint) {
+	console.log("Calling service using auth token: " + $scope.authenticationCookie);
 
-
-
-
-
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
-
-
-function CallService($scope, $http) {
-
-	//TODO consider using config() to setup the $httpProvider and include headers there.
 	var requestConfig = { headers: {
-			'Authorization': '5Ccr7Qs6ZkEHbAgLMlDeg0ykUF%2F1Cb%2FxIcULRaUhRnaKR%2BvInWkbuNH6kv8MOkxYMc1qdWKm1apyxiNQe7cLXuogOCM4XQTHsa7u8BugcJaW53jklD70pSdZrKfYI24%2FwlQS6nxmYVvBM%2BZPZUY2DTbV5qZuvqd3GtUsy47PZahGyIJ6JxUW39hqw5sqHvSIqxsdDNPgUzG7UWvze2uwZh%2Bn2y5YjaBNMFHt3QoPoL5At88Re3P0tNCoMZ92bUAqdkLQnG2YnnTnp6wq%2FufVF6%2BKB82Cd1mwHcb%2BFFGicxyertcPOms5DM4OI7Mz%2F%2B%2B%2F7HkbLgAyH5j%2FJLj8Ig3tbg%3D%3D',
+			'Authorization': $scope.authenticationCookie,
 			'ApplicationId': '36'
 		}
 	};
 
-	var serviceUrl = 'http://services-ext-stg.dnb.com/rest/LinkageService/V2/OrderProduct?DUNSNumber=211528187&CountryISOAlpha2Code=GB&DNBProductID=LNK_FF_MNRT';
-
-    $http.get(serviceUrl, requestConfig).
+    $http.get(endpoint, requestConfig).
 		success(function(data, status, headers, config) {
             $scope.responseBody = data;
 			$scope.responseHeaders = headers();
@@ -158,11 +144,15 @@ function CallService($scope, $http) {
             console.log(status);
             console.log(JSON.stringify(config));
 		});
-
 }
 
 
-
+ /**
+  * The name says it all.
+  */
+function replaceAll(input, target, replacement) {
+	return input.split(target).join(replacement);
+}
 
 
 
