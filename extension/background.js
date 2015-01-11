681acac0f9e16c8a2da2d0e9e2adad06a15f9293
Paul Hitz <hitzp@dnb.com>
2015-01-11 20:10:42 +0000

    Add Google Analytics (incomplete)

--- extension/background.js
+++ extension/background.js
@@ -1,4 +1,24 @@
 
+//Add a flag to indicate Dev mode.
+chrome.browserAction.setBadgeText({text: "Dev"});
+
+
+//Open the extension in a new tab.
 chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.create({ url: "index.html" });
+	chrome.tabs.create({ url: "index.html" });
 });
+
+//Google Analytics
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-266330-4']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script');
+  ga.type = 'text/javascript';
+  ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(ga, s);
+})();
+
