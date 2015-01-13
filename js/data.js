6f7c85a6bd5699798122904ac920b8958b08f82e
Paul Hitz <hitzp@dnb.com>
2015-01-13 15:46:32 +0000

    Add production support

--- js/data.js
+++ js/data.js
@@ -9,6 +9,9 @@ angular.module('clientApp').constant('SERVICES_CONFIG', {
 	}, {
 		id : "stg",
 		label : "Staging"
+	}, {
+		id : "prod",
+		label : "Production (Requires Credentials)"
 	} ],
 
 	services : [ {
@@ -55,6 +58,10 @@ angular.module('clientApp').constant('SERVICES_CONFIG', {
 		service : "auth",
 		url : "http://services-ext-stg.dnb.com/rest/Authentication"
 	}, {
+		env : "prod",
+		service : "auth",
+		url : "https://maxcvservices.dnb.com/rest/Authentication"
+	}, {
 		env : "qa",
 		service : "pcs",
 		url : "http://services-ext-qa.dnb.com/rest/ProductCatalogService/V2/ListAvailableProduct?DUNSNumber={duns}"
@@ -62,6 +69,10 @@ angular.module('clientApp').constant('SERVICES_CONFIG', {
 		env : "stg",
 		service : "pcs",
 		url : "http://services-ext-stg.dnb.com/rest/ProductCatalogService/V2/ListAvailableProduct?DUNSNumber={duns}"
+	}, {
+		env : "prod",
+		service : "pcs",
+		url : "https://maxcvservices.dnb.com/rest/ProductCatalogService/V2/ListAvailableProduct?DUNSNumber={duns}"
 	}, {
 		env : "qa",
 		service : "linkage",
@@ -71,6 +82,10 @@ angular.module('clientApp').constant('SERVICES_CONFIG', {
 		service : "linkage",
 		url : "http://services-ext-stg.dnb.com/rest/LinkageService/V2/OrderProduct?DUNSNumber={duns}&CountryISOAlpha2Code=GB&DNBProductID=LNK_FF_MNRT"
 	}, {
+		env : "prod",
+		service : "linkage",
+		url : "https://maxcvservices.dnb.com/rest/LinkageService/V2/OrderProduct?DUNSNumber={duns}&CountryISOAlpha2Code=GB&DNBProductID=LNK_FF_MNRT"
+	}, {
 		env : "qa",
 		service : "firm",
 		url : "http://services-ext-qa.dnb.com/V2/organizations/{duns}/products/DCP_PREM_ONBRD?ArchiveProductOptOutIndicator=true"
@@ -78,6 +93,10 @@ angular.module('clientApp').constant('SERVICES_CONFIG', {
 		env : "stg",
 		service : "firm",
 		url : "http://services-ext-stg.dnb.com/V2.0/organizations/{duns}/products/DCP_PREM_ONBRD?ArchiveProductOptOutIndicator=true"
+	}, {
+		env : "prod",
+		service : "firm",
+		url : "https://maxcvservices.dnb.com/V2.0/organizations/{duns}/products/DCP_PREM_ONBRD?ArchiveProductOptOutIndicator=true"
 	}, {
 		env : "qa",
 		service : "gbo",
@@ -87,6 +106,10 @@ angular.module('clientApp').constant('SERVICES_CONFIG', {
 		service : "gbo",
 		url : "http://services-ext-stg.dnb.com/V2.0/organizations/{duns}/products/GBO?ArchiveProductOptOutIndicator=true"
 	}, {
+		env : "prod",
+		service : "gbo",
+		url : "https://maxcvservices.dnb.com/V2/organizations/{duns}/products/GBO?ArchiveProductOptOutIndicator=true&ApplicationTransactionID=onboard"
+	}, {
 		env : "qa",
 		service : "so",
 		url : "http://services-ext-qa.dnb.com/V2/organizations/{duns}/products/SMPL_OWNSHP?ArchiveProductOptOutIndicator=true"
@@ -94,6 +117,10 @@ angular.module('clientApp').constant('SERVICES_CONFIG', {
 		env : "stg",
 		service : "so",
 		url : "http://services-ext-stg.dnb.com/V2.0/organizations/{duns}/products/SMPL_OWNSHP?ArchiveProductOptOutIndicator=true"
+	}, {
+		env : "prod",
+		service : "so",
+		url : "https://maxcvservices.dnb.com/V2/organizations/{duns}/products/SMPL_OWNSHP?ArchiveProductOptOutIndicator=true&ApplicationTransactionID=onboard"
 	}, {
 		env : "qa",
 		service : "kyc",
@@ -103,6 +130,10 @@ angular.module('clientApp').constant('SERVICES_CONFIG', {
 		service : "kyc",
 		url : "http://services-ext-stg.dnb.com/V2.0/organizations/{duns}/products/KYC?ArchiveProductOptOutIndicator=true"
 	}, {
+		env : "prod",
+		service : "kyc",
+		url : "https://maxcvservices.dnb.com/V2/organizations/{duns}/products/KYC?ArchiveProductOptOutIndicator=true"
+	}, {
 		env : "qa",
 		service : "rdc_search",
 		url : "http://services-ext-qa.dnb.com/V1.0/compliancecheck/entities/alerts?CandidateDisplayStartSequenceNumber=1&CandidatePerPageMaximumQuantity=100&CustomerBillingEndorsementText=teamjoly&SubjectTypeText=P&subjectname={duns}"
@@ -110,6 +141,10 @@ angular.module('clientApp').constant('SERVICES_CONFIG', {
 		env : "stg",
 		service : "rdc_search",
 		url : "http://services-ext-stg.dnb.com/V1.0/compliancecheck/entities/alerts?CandidateDisplayStartSequenceNumber=1&CandidatePerPageMaximumQuantity=100&CustomerBillingEndorsementText=teamjoly&SubjectTypeText=P&subjectname={duns}"
+	}, {
+		env : "prod",
+		service : "rdc_search",
+		url : "https://maxcvservices.dnb.com/V1.0/compliancecheck/entities/alerts?CandidateDisplayStartSequenceNumber=1&CandidatePerPageMaximumQuantity=100&CustomerBillingEndorsementText=teamjoly&SubjectTypeText=P&subjectname={duns}"
 	}, {
 		env : "qa",
 		service : "match",
@@ -119,6 +154,10 @@ angular.module('clientApp').constant('SERVICES_CONFIG', {
 		service : "match",
 		url : "http://services-ext-stg.dnb.com/rest/CompanyService/V2/Match?CountryISOAlpha2Code=BE&DUNSNumber={duns}"
 	}, {
+		env : "prod",
+		service : "match",
+		url : "https://maxcvservices.dnb.com/rest/CompanyService/V2/Match?CountryISOAlpha2Code=BE&DUNSNumber={duns}"
+	}, {
 		env : "qa",
 		service : "list_portfolio",
 		url : "http://services-ext-qa.dnb.com/rest/PortfolioService/V2/ListPortfolioAsset"
@@ -126,6 +165,10 @@ angular.module('clientApp').constant('SERVICES_CONFIG', {
 		env : "stg",
 		service : "list_portfolio",
 		url : "http://services-ext-stg.dnb.com/rest/PortfolioService/V2/ListPortfolioAsset"
+	}, {
+		env : "prod",
+		service : "list_portfolio",
+		url : "https://maxcvservices.dnb.com/rest/PortfolioService/V2/ListPortfolioAsset"
 	}, {
 		env : "qa",
 		service : "investigation",
@@ -135,6 +178,10 @@ angular.module('clientApp').constant('SERVICES_CONFIG', {
 		service : "investigation",
 		url : "http://services-ext-stg.dnb.com/rest/InvestigationService/V2/PlaceInvestigation"
 	}, {
+		env : "prod",
+		service : "investigation",
+		url : "https://maxcvservices.dnb.com/rest/InvestigationService/V2/PlaceInvestigation"
+	}, {
 		env : "qa",
 		service : "portfolio_assets",
 		url : "http://services-ext-qa.dnb.com/V2/assets?OrderTransactionTypeCode=0&CandidateMaximumQuantity=10&SortBasisText=PortfolioAssetID&SortDirectionText=Ascending&CandidateDisplayStartSequenceNumber=1&ApplicationTransactionID=f9fc47c5-6733-4325-bbbe-157deb0520ba&DNBProductID=ComplianceReport"
@@ -142,6 +189,10 @@ angular.module('clientApp').constant('SERVICES_CONFIG', {
 		env : "stg",
 		service : "portfolio_assets",
 		url : "http://services-ext-stg.dnb.com/V2/assets?OrderTransactionTypeCode=0&CandidateMaximumQuantity=10&SortBasisText=PortfolioAssetID&SortDirectionText=Ascending&CandidateDisplayStartSequenceNumber=1&ApplicationTransactionID=f9fc47c5-6733-4325-bbbe-157deb0520ba&DNBProductID=ComplianceReport"
+	}, {
+		env : "prod",
+		service : "portfolio_assets",
+		url : "https://maxcvservices.dnb.com/V2/assets?OrderTransactionTypeCode=0&CandidateMaximumQuantity=10&SortBasisText=PortfolioAssetID&SortDirectionText=Ascending&CandidateDisplayStartSequenceNumber=1&ApplicationTransactionID=f9fc47c5-6733-4325-bbbe-157deb0520ba&DNBProductID=ComplianceReport"
 	} ],
 
 	//Use this DUNs if none is provided.
