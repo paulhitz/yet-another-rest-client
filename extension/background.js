8e0c0bdf03f981b82b4655f6b75d0f76c4d61619
Paul Hitz <hitzp@dnb.com>
2015-01-13 16:33:36 +0000

    Google Analytics

--- extension/background.js
+++ extension/background.js
@@ -1,6 +1,6 @@
 
 //Add a flag to indicate Dev mode.
-chrome.browserAction.setBadgeText({text: "Dev"});
+//chrome.browserAction.setBadgeText({text: "Dev"});
 
 
 //Open the extension in a new tab.
@@ -8,17 +8,13 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 	chrome.tabs.create({ url: "index.html" });
 });
 
-//Google Analytics
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-266330-4']);
-_gaq.push(['_trackPageview']);
-
-(function() {
-  var ga = document.createElement('script');
-  ga.type = 'text/javascript';
-  ga.async = true;
-  ga.src = 'https://ssl.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0];
-  s.parentNode.insertBefore(ga, s);
-})();
+//Google Analytics.
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-266330-4', 'auto');
+ga('set', 'checkProtocolTask', function(){});
+ga('require', 'displayfeatures');
+ga('send', 'pageview');
 
