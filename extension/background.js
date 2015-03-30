9dff5a3b84aa0dfe7f1512d7ea02378a9ffde746
Paul Hitz <hitzp@dnb.com>
2015-03-30 14:18:30 +0100

    Minor JS-Hint changes

--- extension/background.js
+++ extension/background.js
@@ -4,7 +4,7 @@
 
 
 //Open the extension in a new tab.
-chrome.browserAction.onClicked.addListener(function(tab) {
+chrome.browserAction.onClicked.addListener(function() {
 	chrome.tabs.create({ url: "index.html" });
 });
 
