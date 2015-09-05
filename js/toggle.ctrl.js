b3377b06bd2d4db27583ddba3050937f366a5416
Paul Hitz <hitzp@dnb.com>
2015-09-05 17:56:32 +0100

    Remove JQuery dependency for collapse functionality
    Replace the bootstrap/jquery method with the AngularJS/bootstrap way.

--- js/toggle.ctrl.js
+++
@@ -1,9 +0,0 @@
-
-/**
- * Simple controller for toggling a value.
- */
-clientApp.controller('ToggleCtrl', function($scope) {
-	$scope.toggle = function(status) {
-		$scope.status = !status;
-	};
-});
