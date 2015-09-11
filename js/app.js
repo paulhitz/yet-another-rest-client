6548c72e02b715e7782b16d6aa1fda13c18ef945
Paul Hitz <hitzp@dnb.com>
2015-09-11 19:01:14 +0100

    UI Experimentation

--- js/app.js
+++ js/app.js
@@ -6,40 +6,10 @@ var clientApp = angular.module('clientApp', ['ui.bootstrap', 'hljs', 'common', '
 clientApp.controller('ClientAppCtrl', function($scope, $log, AuthService, clientAppHelper, utils,
 		ProgressbarService, advancedSettings, SERVICES_CONFIG, credentials) {
 		
-		
-		
-		
-  $scope.address = {};
-  $scope.refreshAddresses = function(address) {
 
-      $scope.addresses = [
-    { name: 'Adam2',      email: 'adam@email.com',      age: 10 },
-    { name: 'Amalie2',    email: 'amalie@email.com',    age: 12 },
-    { name: 'Wladimir',  email: 'wladimir@email.com',  age: 30 },
-    { name: 'Samantha',  email: 'samantha@email.com',  age: 31 },
-    { name: 'Estefanía', email: 'estefanía@email.com', age: 16 },
-    { name: 'Natasha',   email: 'natasha@email.com',   age: 54 },
-    { name: 'Nicole',    email: 'nicole@email.com',    age: 43 },
-    { name: 'Adrian2',    email: 'adrian@email.com',    age: 21 }
-  ];
-
-  };
 		
-		
-		
-		  $scope.addresses = [
-    { name: 'Adam',      email: 'adam@email.com',      age: 10 },
-    { name: 'Amalie',    email: 'amalie@email.com',    age: 12 },
-    { name: 'Wladimir',  email: 'wladimir@email.com',  age: 30 },
-    { name: 'Samantha',  email: 'samantha@email.com',  age: 31 },
-    { name: 'Estefanía', email: 'estefanía@email.com', age: 16 },
-    { name: 'Natasha',   email: 'natasha@email.com',   age: 54 },
-    { name: 'Nicole',    email: 'nicole@email.com',    age: 43 },
-    { name: 'Adrian',    email: 'adrian@email.com',    age: 21 }
-  ];
-		
-		
-		
+	$scope.endpoint = {};
+	$scope.endpoints = SERVICES_CONFIG.endpoints;
 		
 		
 		
