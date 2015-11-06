f7bf76a6e3ad5a96c4cfce231a7a2be2316b1e08
Paul Hitz <hitzp@dnb.com>
2015-11-06 17:44:12 +0000

    Add Example Request Header

--- js/data.js
+++ js/data.js
@@ -607,25 +607,37 @@ angular.module('clientApp').value('advancedSettings', {
 angular.module('clientApp').value('credentials', {});
 
 /**
- * TODO need to add a lot more example headers here.
+ * 
  */
 angular.module('clientApp').constant('EXAMPLE_HEADERS', [ 
 	{
-		id : "auth",
-		name : "Authorization",
-		value : ""
+		id : "accept",
+		name : "Accept",
+		value : "application/json"
 	}, {
-		id : "appId",
+		id : "accept_charset",
+		name : "Accept-Charset",
+		value : "utf-8"
+	}, {
+		id : "app_id",
 		name : "ApplicationId",
 		value : "36"
+	}, {
+		id : "auth",
+		name : "Authorization",
+		value : "Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ=="
+	}, {
+		id : "cache",
+		name : "Cache-Control",
+		value : "no-cache"
 	}, {
 		id : "content",
 		name : "Content-Type",
 		value : "application/json"
 	}, {
-		id : "accept",
-		name : "Accept",
-		value : "application/json"
+		id : "user_agent",
+		name : "User-Agent",
+		value : "Mozilla/5.0 (X11; Linux x86_64; rv:12.0) Gecko/20100101 Firefox/21.0"
 	} ]
 );
 
