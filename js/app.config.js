6b3848502743ea8f4583c9148bc2752c1c9e9a9f
Paul Hitz <hitzp@dnb.com>
2016-01-23 17:40:45 +0000

    Fix Chrome App Issues

--- js/app.config.js
+++ js/app.config.js
@@ -2,6 +2,12 @@
 /**
  * Configuration settings.
  */
-clientApp.config(function ($analyticsProvider) {
+clientApp.config(function ($analyticsProvider, $provide) {
 	$analyticsProvider.virtualPageviews(false);
+
+	//Prevent Angular from sniffing for the history API since it's not supported in packaged apps.
+	$provide.decorator('$window', function($delegate) {
+		$delegate.history = null;
+		return $delegate;
+	});
 });
