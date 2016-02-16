6aad189c2c73b1d7194136b57e8689ce976c6c66
Paul Hitz <hitzp@dnb.com>
2016-02-16 18:29:26 +0000

    Revert "resolve merge issue"
    This reverts commit 7eca689b15822ce76ead6026dc9dda5901ae4029, reversing
    changes made to f83e562cb69d0c2e9adccbe34b58253786c26e93.

--- app/background.js
+++ app/background.js
@@ -3,6 +3,5 @@ chrome.app.runtime.onLaunched.addListener(function() {
   // var url = chrome.runtime.getURL('index.html');
   // chrome.browser.openTab({'url': url});
 
-  //Open the app in a new browser tab.
   window.open("index.html");
 });
--- js/app.config.js
+++ js/app.config.js
@@ -2,13 +2,6 @@
 /**
  * Configuration settings.
  */
-clientApp.config(function ($analyticsProvider, $provide) {
+clientApp.config(function ($analyticsProvider) {
 	$analyticsProvider.virtualPageviews(false);
-
-	//Prevent Angular from sniffing for the history API since it's not supported in packaged apps.
-	//@see https://github.com/angular/angular.js/issues/11932
-	$provide.decorator('$window', function($delegate) {
-		Object.defineProperty($delegate, 'history', {get: function(){ return null; }});
-		return $delegate;
-	});
 });
