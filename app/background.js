015a7c5a65402018c08719b5cc445221ac7c15a7
Paul Hitz <hitzp@dnb.com>
2016-02-16 18:35:08 +0000

    Revert "Merge branch 'chrome-app' into develop"
    This reverts commit f83e562cb69d0c2e9adccbe34b58253786c26e93, reversing
    changes made to 2ab515a614a3d3cbbeaf27e5e3734ec0414f2f66.

--- app/background.js
+++
@@ -1,7 +0,0 @@
-
-chrome.app.runtime.onLaunched.addListener(function() {
-  // var url = chrome.runtime.getURL('index.html');
-  // chrome.browser.openTab({'url': url});
-
-  window.open("index.html");
-});
