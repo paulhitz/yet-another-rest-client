47570b79484954cfea59ff143fc9a3b4d8fbad9a
Paul Hitz <hitzp@dnb.com>
2016-02-18 12:11:00 +0000

    Remove dead code and conflicting manifests

--- app/background.js
+++ app/background.js
@@ -4,5 +4,13 @@ chrome.app.runtime.onLaunched.addListener(function() {
   // chrome.browser.openTab({'url': url});
 
   //Open the app in a new browser tab.
-  window.open("index.html");
+  //window.open("index.html");
+
+  //Open the app in a new window.
+  chrome.app.window.create('index.html', {
+    id: 'yarcId',
+    state: "fullscreen",
+    minWidth: 1000,
+    minHeight: 800
+  });
 });
